8079f2a5f2136f664eca1770892276c2d9fe3c52
leonux123 <42426813+leonux123@users.noreply.github.com>
2018-11-26 00:11:44 -0600

    Update ValidateText.java

--- src/test/selenium/src/test/java/selenium/ValidateText.java
+++ src/test/selenium/src/test/java/selenium/ValidateText.java
@@ -23,7 +23,7 @@ public class ValidateText {
 	 String actualTitle = driver.getTitle();
          
         // check_app_version
-	 Assert.assertEquals("Condition true", "PetClinic v2.1", actualStr);
+	 Assert.assertEquals("Condition true", "PetClinic v2.2", actualStr);
 
 	// check_title_text
 	 Assert.assertEquals("Condition true", "PetClinic :: a Spring Framework demonstration", actualTitle);
