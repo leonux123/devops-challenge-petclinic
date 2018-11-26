fae9ed8a4087778d144cc81237b1d5e409d07d82
leonux123 <42426813+leonux123@users.noreply.github.com>
2018-11-25 23:07:06 -0600

    Add files via upload

---
+++ src/test/selenium/src/test/java/selenium/ValidateText.java
@@ -0,0 +1,38 @@
+package selenium;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
+import org.junit.Assert;
+
+public class ValidateText {
+
+	private static WebDriver driver;
+
+	@BeforeClass
+	public static void startDriver() {
+		driver = Utils.getDriver();
+	}
+	
+	@Test
+	public void test_case_1() {
+	 driver.get(Utils.getBaseUrl());
+	 String actualStr = driver.findElement(By.xpath("/html/body/div/div/h3[1]")).getText();
+	 String actualTitle = driver.getTitle();
+         
+        // check_app_version
+	 Assert.assertEquals("Condition true", "PetClinic v2.1", actualStr);
+
+	// check_title_text
+	 Assert.assertEquals("Condition true", "PetClinic :: a Spring Framework demonstration", actualTitle);
+
+	 driver.close();
+	}
+	
+	@AfterClass
+	public static void stopDriver() {
+		driver.quit();
+	}
+}
